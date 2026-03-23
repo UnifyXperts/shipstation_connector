967b5d8c00d4b5750776ddc9b820b552c0a6944f
UnifyXperts <support@unifyxperts.com>
2026-03-23 15:53:56 +0530

    add market_place order id doctype

---
+++ shipstation_connector/shipstation_connector/doctype/marketplace_order_id/marketplace_order_id.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, unifyxperts and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Marketplace Order ID", {
+// 	refresh(frm) {
+
+// 	},
+// });
